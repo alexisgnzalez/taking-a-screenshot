d41817c4012611bd272dc539977fe37334ac660a
Alexis Gonzalez <1991.alexis@gmail.com>
2023-07-03 19:39:48 -0400

    Implemented take screenshot to a poem and added download and share fetures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'taking-a-screenshot';
+  constructor(private deviceService: DeviceDetectorService) {}
+  takeScreenshot() {
+    console.log('screenshoniando...');
+    html2canvas(
+      // @ts-ignore
+      document.querySelector('#capture'),
+      { useCORS: true }
+    ).then((canvas) => {
+      if (this.deviceService.isDesktop()) {
+        this.downloadImage(canvas);
+      } else {
+        this.share(canvas);
+      }
+    });
+  }
+
+  downloadImage(canvas: HTMLCanvasElement) {
+    console.log('Downloading image...');
+    canvas.toBlob((blob) => {
+      if (blob) {
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'irole.jpg';
+        link.click();
+      }
+    }, 'image/jpg');
+  }
+
+  share(canvas: HTMLCanvasElement) {
+    console.log('estoy tratando de compartir via phone');
+    if (navigator.share) {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          const file = new File([blob], 'my-image.png', {
+            type: 'image/png',
+            lastModified: Date.now(),
+          });
+          const share: ShareData = {
+            title: 'The Road Not Taken',
+            text: 'This poem was written by Robert Frost in 1916. It is a classic example of a short poem that is both beautiful and thought-provoking.',
+            files: [file],
+          };
+          navigator.share(share).then(
+            () => {
+              console.log('Sharing was successful');
+            },
+            (error) => {
+              console.log('Sharing failed', error);
+            }
+          );
+        }
+      }, 'image/png');
+    } else {
+      console.log('Browser does not support the Web Share API');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PoemComponent } from './components/poem/poem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PoemComponent
   ],
   imports: [
     BrowserModule
